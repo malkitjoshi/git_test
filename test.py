7f57f6d287b33df9ec9ab4f04c9fdf3bc7b0c33c
Malkit Joshi <malkitjoshi@Malkits-MacBook-Air.local>
2019-08-17 14:38:38 +0530

    modified test with square

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 def addone(num):
     return num+1
 
+def square(num):
+    return num**2
+
 if __name__ == "__main__":
     print(addone(4))
 
-def multitwo(num):
-    return num*2
 
-if __name__ == "__main__":
-    print(multitwo(8))
-    
-print("Check pull request")
+
